262b042bc57ae97405d2653de1310e34d14c5b4e
luqmanamirullah <amirullahluqman@gmail.com>
2023-09-03 11:08:23 +0700

    fix: date formatter

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -6,128 +6,129 @@ use Illuminate\Http\Request;
 
 class BeritaController extends Controller
 {
-    public function index()
-    {
-    	return view('admin.berita.berita');
-    }
+	public function index()
+	{
+		// dd('test');
+		return view('admin.berita.berita');
+	}
 
-    public function show_all()
-    {
-    	return view('berita.show_all', [
-    		'data' => \App\Berita::orderBy('created_at', 'DESC')->get()
-    	]);
-    }
+	public function show_all()
+	{
+		return view('berita.show_all', [
+			'data' => \App\Berita::orderBy('created_at', 'DESC')->get()
+		]);
+	}
 
-    public function show($slug='')
-    {
-        $berita = \App\Berita::where('slug', $slug)->first();
-        $berita->update([
-            'hits' => $berita->hits += 1
-        ]);
-    	return view('berita.index', [
-    		'berita' => $berita
-    	]);
-    }
+	public function show($slug = '')
+	{
+		$berita = \App\Berita::where('slug', $slug)->first();
+		$berita->update([
+			'hits' => $berita->hits += 1
+		]);
+		return view('berita.index', [
+			'berita' => $berita
+		]);
+	}
 
-    public function create()
-    {
-    	return view('admin.berita.berita_form');
-    }
+	public function create()
+	{
+		return view('admin.berita.berita_form');
+	}
 
-    public function store(Request $request)
-    {
-    	$this->validate($request, $this->rules());
+	public function store(Request $request)
+	{
+		$this->validate($request, $this->rules());
 
-    	$input = $request->all();
+		$input = $request->all();
 
-    	$foto = '';
-
-		if ($request->hasFile('foto') && $request->file('foto')->isValid()) {
-    		$foto = \Carbon\Carbon::now()->format('Y-m-d-H-i-s') . ' ' . $input['judul'];
-    		$foto = str_slug($foto, '-');
-    		$foto .=  '.' . $request->file('foto')->getClientOriginalExtension();
-    		$request->file('foto')->storeAs('', $foto);
-    		$data['foto'] = $foto;
-    	}
-
-    	$slug = str_slug($input['judul'], '-');
-
-    	$berita = \App\Berita::where('slug', $slug)->get();
-    	if ($berita->count()) {
-    		$slug .= '-';
-    		$slug .= $berita->count() + 1;
-    	}
-
-    	$data = [
-    		'judul' => $input['judul'],
-    		'deskripsi' => $input['deskripsi'],
-    		'foto' => $foto,
-    		'slug' => $slug,
-    		'id_kategori' => $input['id_kategori'],
-    		'hits' => 0,
-    	];
-
-        if ($input['created_at']) {
-            $data['created_at'] = $input['created_at'];
-        }
-
-    	\App\Berita::create($data);
-
-    	return redirect('admin/berita')->with('success', 'Berhasil Menambahkan Berita');
-    }
-
-    public function edit($id='')
-    {
-    	return view('admin.berita.berita_form', [
-    		'berita' => \App\Berita::find($id)
-    	]);
-    }
-
-    public function update($id='', Request $request)
-    {
-    	$this->validate($request, $this->rules());
-
-    	$input = $request->all();
-
-    	$data = [
-    		'judul' => $input['judul'],
-    		'deskripsi' => $input['deskripsi'],
-    		'id_kategori' => $input['id_kategori'],
-    	];
+		$foto = '';
 
 		if ($request->hasFile('foto') && $request->file('foto')->isValid()) {
-    		$foto = \Carbon\Carbon::now()->format('Y-m-d-H-i-s') . ' ' . $input['judul'];
-    		$foto = str_slug($foto, '-');
-    		$foto .=  '.' . $request->file('foto')->getClientOriginalExtension();
-    		$request->file('foto')->storeAs('', $foto);
-    		$data['foto'] = $foto;
-    	}
+			$foto = \Carbon\Carbon::now()->format('Y-m-d-H-i-s') . ' ' . $input['judul'];
+			$foto = str_slug($foto, '-');
+			$foto .= '.' . $request->file('foto')->getClientOriginalExtension();
+			$request->file('foto')->storeAs('', $foto);
+			$data['foto'] = $foto;
+		}
 
-        if ($input['created_at']) {
-            $data['created_at'] = $input['created_at'];
-        }
+		$slug = str_slug($input['judul'], '-');
 
-    	$berita = \App\Berita::find($id);
-    	$berita->update($data);
-    	return redirect('admin/berita')->with('success', 'Berhasil Mengubah Berita');
-    }
+		$berita = \App\Berita::where('slug', $slug)->get();
+		if ($berita->count()) {
+			$slug .= '-';
+			$slug .= $berita->count() + 1;
+		}
 
-    public function destroy($id)
-    {
-    	$berita = \App\Berita::find($id);
-        \Storage::delete($berita->foto);
-        $berita->delete();
-    	return response()->json($berita);
-    }
+		$data = [
+			'judul' => $input['judul'],
+			'deskripsi' => $input['deskripsi'],
+			'foto' => $foto,
+			'slug' => $slug,
+			'id_kategori' => $input['id_kategori'],
+			'hits' => 0,
+		];
 
-    public function rules()
-    {
-    	return [
-    		'judul' => 'required|max:150',
-    		'deskripsi' => 'required',
-    		'foto' => 'mimes:jpeg,png|max:100000',
-    		'id_kategori' => 'required|exists:kategori',
-            'created_at' => 'nullable|date'
-    	];
-    }
+		if ($input['created_at']) {
+			$data['created_at'] = $input['created_at'];
+		}
+
+		\App\Berita::create($data);
+
+		return redirect('admin/berita')->with('success', 'Berhasil Menambahkan Berita');
+	}
+
+	public function edit($id = '')
+	{
+		return view('admin.berita.berita_form', [
+			'berita' => \App\Berita::find($id)
+		]);
+	}
+
+	public function update($id = '', Request $request)
+	{
+		$this->validate($request, $this->rules());
+
+		$input = $request->all();
+
+		$data = [
+			'judul' => $input['judul'],
+			'deskripsi' => $input['deskripsi'],
+			'id_kategori' => $input['id_kategori'],
+		];
+
+		if ($request->hasFile('foto') && $request->file('foto')->isValid()) {
+			$foto = \Carbon\Carbon::now()->format('Y-m-d-H-i-s') . ' ' . $input['judul'];
+			$foto = str_slug($foto, '-');
+			$foto .= '.' . $request->file('foto')->getClientOriginalExtension();
+			$request->file('foto')->storeAs('', $foto);
+			$data['foto'] = $foto;
+		}
+
+		if ($input['created_at']) {
+			$data['created_at'] = $input['created_at'];
+		}
+
+		$berita = \App\Berita::find($id);
+		$berita->update($data);
+		return redirect('admin/berita')->with('success', 'Berhasil Mengubah Berita');
+	}
+
+	public function destroy($id)
+	{
+		$berita = \App\Berita::find($id);
+		\Storage::delete($berita->foto);
+		$berita->delete();
+		return response()->json($berita);
+	}
+
+	public function rules()
+	{
+		return [
+			'judul' => 'required|max:150',
+			'deskripsi' => 'required',
+			'foto' => 'mimes:jpeg,png|max:100000',
+			'id_kategori' => 'required|exists:kategori',
+			'created_at' => 'nullable|date'
+		];
+	}
 }
--- resources/views/admin/berita/berita.blade.php
+++ resources/views/admin/berita/berita.blade.php
@@ -1,101 +1,113 @@
 @extends('admin.header')
 @section('title', 'Berita')
 @section('content')
-<div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
-    <div class="page-header pull-left">
-        <div class="page-title">@yield('title')</div>
+    <div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
+        <div class="page-header pull-left">
+            <div class="page-title">@yield('title')</div>
+        </div>
+        <ol class="breadcrumb page-breadcrumb">
+            <li>
+                <i class="fa fa-home"></i>&nbsp;<a href="index.html">Home</a>&nbsp;&nbsp;<i
+                    class="fa fa-angle-right"></i>&nbsp;&nbsp;
+            </li>
+            <li>
+                <a href="#">Admin</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;
+            </li>
+            <li class="active">
+                @yield('title')
+            </li>
+        </ol>
+        <div class="clearfix"></div>
     </div>
-    <ol class="breadcrumb page-breadcrumb">
-        <li>
-            <i class="fa fa-home"></i>&nbsp;<a href="index.html">Home</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;
-        </li>
-        <li>
-        <a href="#">Admin</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
-        <li class="active">
-            @yield('title')
-        </li>
-    </ol>
-    <div class="clearfix"></div>
-</div>
-<div class="page-content">
+    <div class="page-content">
 
-    @include('admin.error')
+        @include('admin.error')
 
-	<!-- BEGIN MAIN CONTENT -->
-    <div class="row">
-    	<div class="col-xs-12">
-    		<div class="panel panel-default">
-    			<div class="panel-heading">
-    				Data Berita
-    			</div>
-				<div class="panel-body">
-					<p><a href="{{ url('admin/berita/create') }}" class="btn btn-primary"><i class="fa fa-plus"></i> Tambah Berita</a></p>
-			    	<table class="table table-bordered">
-			    		<thead>
-			    			<tr>
-				    			<th>No</th>
-				    			<th>Judul</th>
-				    			<th>Tanggal</th>
-				    			<th>Action</th>
-			    			</tr>
-			    		</thead>
-			    		<tbody>
-			    			@foreach (\App\Berita::orderBy('created_at', 'DESC')->get() as $berita)
-			    			<tr>
-			    				<td>{{ @$i ? $i += 1 : $i = 1 }}</td>
-			    				<td>{{ $berita->judul }}</td>
-			    				<td>{{ $berita->created_at->format('l, d F Y - H:i') }}</td>
-			    				<td>
-			    					<form action="{{ url('admin/berita/' . $berita->id_berita) }}" method="POST">
-				    					<a href="{{ url('admin/berita/' . $berita->id_berita . '/edit') }}" class="btn btn-warning"><i class="fa fa-pencil"></i></a>
-				    					<button class="btn btn-danger btn-delete" type="submit"><i class="fa fa-trash"></i></button>
-			    					</form>
-			    				</td>
-			    			</tr>
-			    			@endforeach
-			    		</tbody>
-			    	</table>
-				</div>
-			</div>
-    	</div>
-    </div>
-	<!-- END MAIN CONTENT -->
+        <!-- BEGIN MAIN CONTENT -->
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Data Berita
+                    </div>
+                    <div class="panel-body">
+                        <p><a href="{{ url('admin/berita/create') }}" class="btn btn-primary"><i class="fa fa-plus"></i>
+                                Tambah Berita</a></p>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Judul</th>
+                                    <th>Tanggal</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach (\App\Berita::orderBy('created_at', 'DESC')->get() as $berita)
+                                    <tr>
+                                        <td>{{ @$i ? ($i += 1) : ($i = 1) }}</td>
+                                        <td>{{ $berita->judul }}</td>
+                                        <td>
+                                            @if ($berita->created_at)
+                                                {{ $berita->created_at->format('l, d F Y - H:i') }}
+                                        </td>
+                                    @else
+                                        -
+                                @endif
+                                <td>
+                                    <form action="{{ url('admin/berita/' . $berita->id_berita) }}" method="POST">
+                                        <a href="{{ url('admin/berita/' . $berita->id_berita . '/edit') }}"
+                                            class="btn btn-warning"><i class="fa fa-pencil"></i></a>
+                                        <button class="btn btn-danger btn-delete" type="submit"><i
+                                                class="fa fa-trash"></i></button>
+                                    </form>
+                                </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END MAIN CONTENT -->
 
-</div><!-- END PAGE CONTENT -->
+    </div><!-- END PAGE CONTENT -->
 @endsection
 
 @push('css')
 @endpush
 @push('js')
-<script type="text/javascript">
-	$(document).ready(function() {
-		$('.btn-delete').click(function(e) {
-			e.preventDefault();
-			var url = $(this).parent().attr('action');
-			var tr = $(this).parent().parent().parent();
-			swal({
-				title: "Are you sure?",
-				text: "Your will not be able to recover this imaginary file!",
-				type: "warning",
-				showCancelButton: true,
-				confirmButtonClass: "btn-danger",
-				confirmButtonText: "Yes, delete it!",
-				closeOnConfirm: false
-			},
-			function(){
-				$.ajax({
-					data: {
-						'_token' : '{{ csrf_token() }}',
-						'_method' : 'DELETE',
-					},
-					url: url,
-					method: 'POST',
-				}).done(function(result) {
-					swal("Deleted!", "Your imaginary file has been deleted.", "success");
-					tr.hide();
-				});
-			});
-		});
-	});
-</script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.btn-delete').click(function(e) {
+                e.preventDefault();
+                var url = $(this).parent().attr('action');
+                var tr = $(this).parent().parent().parent();
+                swal({
+                        title: "Are you sure?",
+                        text: "Your will not be able to recover this imaginary file!",
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonClass: "btn-danger",
+                        confirmButtonText: "Yes, delete it!",
+                        closeOnConfirm: false
+                    },
+                    function() {
+                        $.ajax({
+                            data: {
+                                '_token': '{{ csrf_token() }}',
+                                '_method': 'DELETE',
+                            },
+                            url: url,
+                            method: 'POST',
+                        }).done(function(result) {
+                            swal("Deleted!", "Your imaginary file has been deleted.",
+                                "success");
+                            tr.hide();
+                        });
+                    });
+            });
+        });
+    </script>
 @endpush

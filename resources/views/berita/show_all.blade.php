b4711c41dd0a3f4a5ed829c8b348b092e1c976f1
luqmanamirullah <amirullahluqman@gmail.com>
2023-09-05 22:11:34 +0700

    fix: handle nullable timestamps

--- resources/views/berita/show_all.blade.php
+++ resources/views/berita/show_all.blade.php
@@ -12,21 +12,30 @@
             <div class="row">
                 @if ($data->count())
                     @foreach ($data as $berita)
-                    <div class="col-12 col-md-4">
-                        <div class="card">
-                            <img class="card-img-top" src="{{ asset( @$berita->foto ? 'uploads/' . $berita->foto : 'image/default-img.jpg' ) }}" alt="{{ $berita->judul }}">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $berita->judul }}</h5>
-                                <p class="card-text">{{ $berita->created_at->format('d F Y') }}</p>
-                                @php
-                                $deskripsi = strip_tags($berita->deskripsi);
-                                $deskripsi = trim(str_replace('&nbsp;', '', $deskripsi));
-                                @endphp
-                                <p class="card-text">{{ substr($deskripsi, 0, 42) }}{{ strlen($deskripsi) > 42 ? '...' : '' }}</p>
-                                <a href="{{ url('berita/' . $berita->slug) }}" class="">Baca Selengkapnya</a>
+                        <div class="col-12 col-md-4">
+                            <div class="card">
+                                <img class="card-img-top"
+                                    src="{{ asset(@$berita->foto ? 'uploads/' . $berita->foto : 'image/default-img.jpg') }}"
+                                    alt="{{ $berita->judul }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $berita->judul }}</h5>
+                                    <p class="card-text">
+                                        @if ($berita->created_at)
+                                            {{ $berita->created_at->format('d F Y') }}
+                                        @else
+                                            -
+                                        @endif
+                                    </p>
+                                    @php
+                                        $deskripsi = strip_tags($berita->deskripsi);
+                                        $deskripsi = trim(str_replace('&nbsp;', '', $deskripsi));
+                                    @endphp
+                                    <p class="card-text">
+                                        {{ substr($deskripsi, 0, 42) }}{{ strlen($deskripsi) > 42 ? '...' : '' }}</p>
+                                    <a href="{{ url('berita/' . $berita->slug) }}" class="">Baca Selengkapnya</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     @endforeach
                 @else
                     <div class="col-12">
@@ -48,19 +57,24 @@
             padding-top: 86px;
             background-color: #f1f1f1;
         }
+
         .berita {
             padding: 2em 0 4em;
         }
+
         .card {
-            box-shadow: 0 1px 2px rgba(0,0,0,.1);
+            box-shadow: 0 1px 2px rgba(0, 0, 0, .1);
         }
+
         .card img,
         .card {
             border: 0;
         }
+
         .card-body {
             padding: 1.5rem 2rem 3rem;
         }
+
         .card-text {
             text-align: justify;
         }
